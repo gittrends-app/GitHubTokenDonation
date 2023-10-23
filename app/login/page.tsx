3af5c10acb13efa731282e4aa211f51415c136b4
joao.v.gazola <joao.v.gazola@gmail.com>
2023-10-23 15:10:24 -0400

    login static

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,8 +1,18 @@
+'use client'
 import * as React from 'react';
 import Box from '@mui/material/Box';
-import { TextField } from '@mui/material';
+import { Button, FilledInput, FormControl, Grid, IconButton, InputAdornment, InputLabel, TextField, Typography } from '@mui/material';
+import Visibility from '@mui/icons-material/Visibility';
+import VisibilityOff from '@mui/icons-material/VisibilityOff';
 
 export default function LoginPage() {
+  const [showPassword, setShowPassword] = React.useState(false);
+
+  const handleClickShowPassword = () => setShowPassword((show) => !show);
+
+  const handleMouseDownPassword = (event: React.MouseEvent<HTMLButtonElement>) => {
+    event.preventDefault();
+  };
   return (
     <Box
       sx={{
@@ -12,7 +22,47 @@ export default function LoginPage() {
         alignItems: 'center',
       }}
     >
-      <TextField fullWidth label="User" id="user" />
+      <Typography variant='h2' color='primary'>Login</Typography>
+      <br />
+      <Grid container spacing={2}>
+        <Grid item xs={4}></Grid>
+        <Grid item xs={4}>
+          <TextField id="user" fullWidth label="User" variant="filled" />
+        </Grid>
+        <Grid item xs={4}></Grid>
+        <Grid item xs={4}></Grid>
+        <Grid item xs={4}>
+          <FormControl variant="filled"
+            fullWidth>
+            <InputLabel htmlFor="password">Password</InputLabel>
+            <FilledInput
+              id="password"
+              type={showPassword ? 'text' : 'password'}
+              endAdornment={
+                <InputAdornment position="end">
+                  <IconButton
+                    aria-label="Ver senha"
+                    onClick={handleClickShowPassword}
+                    onMouseDown={handleMouseDownPassword}
+                    edge="end"
+                  >
+                    {showPassword ? <VisibilityOff /> : <Visibility />}
+                  </IconButton>
+                </InputAdornment>
+              }
+            />
+          </FormControl>
+        </Grid>
+        <Grid item xs={4}></Grid>
+        <Grid item xs={4}></Grid>
+        <Grid item xs={4}>
+          <Button variant="contained" fullWidth size='large'>Logar</Button>
+        </Grid>
+        <Grid item xs={4}></Grid>
+      </Grid>
+      <div >
+        <br />
+      </div>
     </Box>
   );
 }
